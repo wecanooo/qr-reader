b9ec03f30708041ebb7d3903598bfa594ac0a48b
wecanooo <wecanooo@gmail.com>
2020-12-21 12:00:24 +0900

    최초커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import QrReader from 'react-qr-reader'
 
 function App() {
+  const [data, setData] = React.useState('')
+
+  const handleError = err => {
+    console.error(err)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={data => setData(data)}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
     </div>
   );
 }
